49f44bf7ca32835d9d53b27954e8ede8b22997ef
agoodman1999 <agoodman@civiconnect.ca>
2023-08-25 13:36:22 -0400

    update nginx to use prod domain

---
+++ ansible/templates/get-infisical-secrets.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#fix path substitution https://stackoverflow.com/questions/7250130/how-to-stop-mingw-and-msys-from-mangling-path-names-given-at-the-command-line/34386471#34386471
+export MSYS_NO_PATHCONV=1
