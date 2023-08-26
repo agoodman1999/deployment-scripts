391b40265ef97ad3a29e2e0a088ec53599f8002d
agoodman1999 <agoodman@civiconnect.ca>
2023-08-26 14:28:33 -0400

    update

--- ansible/templates/run-strapi.sh
+++ ansible/templates/run-strapi.sh
@@ -1,4 +1,5 @@
 export INFISICAL_TOKEN="{{ INFISICAL_TOKEN_PRODUCTION_STRAPI}}"
 export INFISICAL_API_URL="https://infisical.civiconnect.net/api"
+export NODE_ENV=production
 infisical run --env=prod --path="/strapi" -- npm run build
 infisical run --env=prod --path="/strapi" -- npm run start
