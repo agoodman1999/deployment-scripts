d0e313602b6ea1d49a2850c502e6f39fa8418980
agoodman1999 <agoodman@civiconnect.ca>
2023-08-26 13:08:08 -0400

    update

---
+++ ansible/templates/backup-strapi-dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export INFISICAL_TOKEN="{{ INFISICAL_TOKEN_DEVELOPMENT_STRAPI }}"
+export INFISICAL_API_URL="https://infisical.civiconnect.net/api"
+
+instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id | cut -d '-' -f 2)
+file_name="$1-$instance_id"
+
+cd /home/bitnami/strapi-dev
+infisical run --env=dev --path="/strapi" -- npm run strapi export -- --no-encrypt --file ../"$file_name"
+aws s3 cp ../"$file_name".tar.gz s3://civiconnect-automated-strapi-backups/"$file_name"-dev.tar.gz
+rm ../"$file_name".tar.gz
